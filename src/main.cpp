9799edfcda7340ae66271220928e2fb436f2d8d5
arlsan00099 <itsfabulous2058@gmail.com>
2024-11-05 17:41:36 +0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,226 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Arduino_JSON.h>
+#include "index.html"
+
+// Replace with your network credentials
+const char* ssid = "STP 3";
+const char* password = "STP@225588";
+
+
+
+
+AsyncWebServer server(80);
+AsyncEventSource events("/events");
+
+JSONVar readings;
+
+// Timer variables
+unsigned long lastTime = 0;  
+unsigned long lastTimeTemperature = 0;
+unsigned long lastTimeAcc = 0;
+unsigned long gyroDelay = 10;
+unsigned long temperatureDelay = 1000;
+unsigned long accelerometerDelay = 200;
+
+// Create a sensor object
+Adafruit_MPU6050 mpu;
+
+sensors_event_t a, g, temp;
+
+
+
+float one=0.11;
+float two =0.22;
+float three=0.33;
+float four =0.44;
+float five=0.55;
+float six=0.66;
+
+float gyroX, gyroY, gyroZ;
+float accX, accY, accZ;
+float temperature;
+
+//Gyroscope sensor deviation
+float gyroXerror = 0.07;
+float gyroYerror = 0.03;
+float gyroZerror = 0.01;
+
+/*
+String getGyroReadings(){
+  mpu.getEvent(&a, &g, &temp);
+
+  float gyroX_temp = g.gyro.x;
+  if(abs(gyroX_temp) > gyroXerror)  {
+    gyroX += gyroX_temp/50.00;
+  }
+  
+  float gyroY_temp = g.gyro.y;
+  if(abs(gyroY_temp) > gyroYerror) {
+    gyroY += gyroY_temp/70.00;
+  }
+
+  float gyroZ_temp = g.gyro.z;
+  if(abs(gyroZ_temp) > gyroZerror) {
+    gyroZ += gyroZ_temp/90.00;
+  }
+
+  readings["gyroX"] = String(gyroX);
+  readings["gyroY"] = String(gyroY);
+  readings["gyroZ"] = String(gyroZ);
+
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
+
+String getAccReadings() {
+  mpu.getEvent(&a, &g, &temp);
+  // Get current acceleration values
+  accX = a.acceleration.x;
+  accY = a.acceleration.y;
+  accZ = a.acceleration.z;
+  readings["accX"] = String(accX);
+  readings["accY"] = String(accY);
+  readings["accZ"] = String(accZ);
+
+  readings["one"] = String(one);
+  readings["two"] = String(two);
+  readings["three"] = String(three);
+
+    readings["four"] = String(four);
+  readings["five"] = String(five);
+  readings["six"] = String(six);
+  
+  String accString = JSON.stringify (readings);
+  return accString;
+}
+
+String getTemperature(){
+  mpu.getEvent(&a, &g, &temp);
+  temperature = temp.temperature;
+  return String(temperature);
+}
+*/
+
+String getGyroReadings() {
+  // Replace sensor readings with dummy values
+  readings["gyroX"] = "1.23";  // Dummy value for gyroX
+  readings["gyroY"] = "4.56";  // Dummy value for gyroY
+  readings["gyroZ"] = "7.89";  // Dummy value for gyroZ
+
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
+
+String getAccReadings() {
+  // Replace sensor readings with dummy values
+  readings["accX"] = "0.12";   // Dummy value for accX
+  readings["accY"] = "3.45";   // Dummy value for accY
+  readings["accZ"] = "6.78";   // Dummy value for accZ
+
+  readings["one"] = "0.11";    // Dummy value for one
+  readings["two"] = "0.22";    // Dummy value for two
+  readings["three"] = "0.33";  // Dummy value for three
+  readings["four"] = "0.44";   // Dummy value for four
+  readings["five"] = "0.55";   // Dummy value for five
+  readings["six"] = "0.66";    // Dummy value for six
+
+  String accString = JSON.stringify(readings);
+  return accString;
+}
+
+String getTemperature() {
+  // Replace temperature reading with a dummy value
+  return "25.0";  // Dummy temperature value
+}
+
+void initMPU(){
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+}
+
+
+
+void setup(){
+  
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+
+ //initMPU();
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+
+    server.on("/reset", HTTP_GET, [](AsyncWebServerRequest *request){
+    gyroX=0;
+    gyroY=0;
+    gyroZ=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+   server.on("/resetX", HTTP_GET, [](AsyncWebServerRequest *request){
+    gyroX=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/resetY", HTTP_GET, [](AsyncWebServerRequest *request){
+    gyroY=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/resetZ", HTTP_GET, [](AsyncWebServerRequest *request){
+    gyroZ=0;
+    request->send(200, "text/plain", "OK");
+  });
+
+ // Handle Web Server Events
+  events.onConnect([](AsyncEventSourceClient *client){
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+    // send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+
+  // Start server
+  server.begin();
+}
+
+void loop() {
+if ((millis() - lastTime) > gyroDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getGyroReadings().c_str(),"gyro_readings",millis());
+    lastTime = millis();
+  }
+  if ((millis() - lastTimeAcc) > accelerometerDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getAccReadings().c_str(),"accelerometer_readings",millis());
+    lastTimeAcc = millis();
+  }
+  if ((millis() - lastTimeTemperature) > temperatureDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getTemperature().c_str(),"temperature_reading",millis());
+    lastTimeTemperature = millis();
+  }
+}
